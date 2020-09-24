71870c7622c7eedb9a5f2c6f5a2c5c781defe6af
rolando-R <landon3388@gmail.com>
2020-09-24 17:32:38 -0500

    update

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,50 @@
-const e = require("express");
 const jwt = require("jsonwebtoken");
 
 require("../config/config");
 
+//=========================
 // VERIFICA Y VALIDA TOKEN
-exports.verificaToken =  (req, res, next) => {
+//=========================
+exports.verificaToken = (req, res, next) => {
   let token = req.get("Authorization");
   if (!token) {
     res.send({ ok: false, message: "token no existe" });
   }
 
-// PRIMERA FORMA DE HACER
-//   const userToken = jwt.verify(token, process.env.SECRET);
-//   if (!userToken) {
-//     res.send(userToken);
-//   }
+  // //PRIMERA FORMA DE HACER
+  //   const userToken = jwt.verify(token, process.env.SECRET);
+  //   if (!userToken) {
+  //     const err = new Error("no autenticado")
+  //     err.statusCode = 401
+  //     throw err
+  //   }
 
-
-
-// SEGUNDA FORMA DE HACER
-jwt.verify(token, process.env.SECRET, (err, usuarioDecoded) => {
+  // SEGUNDA FORMA DE HACER
+  jwt.verify(token, process.env.SECRET, (err, usuarioDecoded) => {
     if (err) {
-      res.send({ ok: err, message: "token no es valido"});
+      res.send({ ok: err, message: "token no es valido" });
+      throw err;
     } else {
-        console.log(usuarioDecoded)
+      console.log(usuarioDecoded);
+      req.usuario = usuarioDecoded
     }
-})
+  });
   next();
 };
+
+//==============================
+// AUTHORIZATION PARA USER ADMIN
+//==============================
+exports.verificaAdmin_Role = (req, res, next) => {
+  let usuario = req.usuario
+  if(usuario.role == "ADMIN_ROLE") {
+    next()
+  } else {
+    return res.json({
+      ok: false,
+      err: {
+        message: "el user no es Administrador"
+      }
+    })
+  }
+}
--- src/routers/usuario.js
+++ src/routers/usuario.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 //MIDDLEWARE DE AUTHORIZATION
-const {verificaToken} = require('../middlewares/auth');
+const {verificaToken, verificaAdmin_Role} = require('../middlewares/auth');
 
 
 const router = Router();
@@ -9,7 +9,7 @@ const { getUsuarios, postUsuario, updateUsuario, deleteUsuario } = require("../c
 
 router.get("/usuarios", verificaToken, getUsuarios);
 router.post('/usuarios', postUsuario)
-router.put('/usuarios/:IdUsuario', updateUsuario)
+router.put('/usuarios/:IdUsuario', [verificaToken, verificaAdmin_Role],  updateUsuario)
 router.delete('/usuarios/:IdUsuario', verificaToken, deleteUsuario)
 
 module.exports = router;
